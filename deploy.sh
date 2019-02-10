4d19e9c671940d02c391e89d2f9b250a864035e2
Deepesh Aggarwal <deepeshnith47@gmail.com>
2019-02-10 14:53:25 -0800

    some more clean up

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,18 @@
+# This part will be used to test before merging the code from another branch to master
 # STAGING_BRANCH="deploy"
-PRODUCTION_BRANCH="master"
 
+# NODE_ENV="master"
 # Determine the environment to deploy to based on which branch this commit is on
-NODE_ENV='staging-test'
-if [[ $TRAVIS_BRANCH == $PRODUCTION_BRANCH ]]; then
-  NODE_ENV="master"
-# elif [[ $TRAVIS_BRANCH == $STAGING_BRANCH ]]; then
-#   NODE_ENV="staging"
-# else
-#   # Don't want to deploy if it's not one of the above branches
-#   echo "Not deploying"
-#   exit
+# NODE_ENV='staging-test'
+
+PRODUCTION_BRANCH="master"
+if [[ $TRAVIS_BRANCH != $PRODUCTION_BRANCH ]]; then
+  # Don't want to deploy if it's not master
+  echo "Not deploying"
+  exit
 fi
 
-EB_ENV="$NODE_ENV"
+EB_ENV="master"
 echo "Deploying to $EB_ENV"
 
 pip install --user --upgrade awsebcli
